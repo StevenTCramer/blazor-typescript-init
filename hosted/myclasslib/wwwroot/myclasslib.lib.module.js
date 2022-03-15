5560297a12247007f7751ca63735f5a16bf441eb
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-15 04:53:51 +0000

    remove js file becuase we will use ts instead.

--- hosted/myclasslib/wwwroot/myclasslib.lib.module.js
+++
@@ -1,7 +0,0 @@
-﻿export function beforeStart(options, extensions) {
-    console.log("****beforeStart****");
-}
-
-export function afterStarted(blazor) {
-    console.log("****afterStarted****");
-}
